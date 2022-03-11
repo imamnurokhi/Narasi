c48de2a828d8021268e368361b88b5f7442f47ae
M. Imam Nurokhi <101321842+imamnurokhi@users.noreply.github.com>
2022-03-11 11:01:08 +0700

    Update user service

--- service-user/src/app.module.ts
+++ service-user/src/app.module.ts
@@ -3,8 +3,9 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { DatabaseModule } from './core/database/database.module';
 import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
-  imports: [AuthModule, UserModule, DatabaseModule, ConfigModule.forRoot({ isGlobal: true }), DatabaseModule],
+  imports: [AuthModule, UserModule, DatabaseModule, ConfigModule.forRoot({ isGlobal: true }), DatabaseModule, UsersModule],
 })
 export class AppModule {}
--- service-user/src/core/constants/index.ts
+++ service-user/src/core/constants/index.ts
@@ -1,3 +1,4 @@
+export const USER_REPOSITORY = 'USER_REPOSITORY';
 export const SEQUELIZE = 'SEQUELIZE';
 export const DEVELOPMENT = 'development';
 export const TEST = 'test';
