400cdd07b0f80b835bfad278f5c2a6d06e9407fc
M. Imam Nurokhi <101321842+imamnurokhi@users.noreply.github.com>
2022-03-11 10:44:10 +0700

    Add interface for db

--- service-user/src/app.module.ts
+++ service-user/src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { DatabaseModule } from './core/database/database.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, UserModule, DatabaseModule],
+  imports: [AuthModule, UserModule, DatabaseModule, ConfigModule.forRoot({ isGlobal: true })],
 })
 export class AppModule {}
